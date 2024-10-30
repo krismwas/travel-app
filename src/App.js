6d0cca3122a2d43a445b9d0485b3f2c21141956a
kris <krismwasgit@gmail.com>
2024-10-30 16:18:42 +0300

    handling data

--- src/App.js
+++ src/App.js
@@ -23,10 +23,15 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
