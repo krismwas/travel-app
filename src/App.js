6e57f5ae18498ee3d9aeecb2744cc520ed4f0c8a
kris <krismwasgit@gmail.com>
2024-10-30 12:52:26 +0300

    components combination

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div>React</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <ParkingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🎋 Far away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😘 trip</h3>
+    </div>
+  );
+}
+
+function ParkingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em> 👜 You have X items on your list, and you already parked X (X%)</em>
+    </footer>
+  );
 }
