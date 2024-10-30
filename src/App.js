afe758ce884e0f6d52a21baef2b84a590295ce56
kris <krismwasgit@gmail.com>
2024-10-30 14:29:16 +0300

    form submit event

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😘 trip</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((el) => (
@@ -30,6 +34,8 @@ function Form() {
           </option>
         ))}
       </select>
+      <input type="text" placeholder="item ..." />
+      <button>add</button>
     </form>
   );
 }
